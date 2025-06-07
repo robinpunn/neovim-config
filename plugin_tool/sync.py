a1581ea2927a0a7da709890f4107e49eb13e3385
pop <robin.punnoose@protonmail.com>
2025-06-07 00:18:55 -0500

    refactored sync to use util function

--- plugin_tool/sync.py
+++ plugin_tool/sync.py
@@ -1,5 +1,4 @@
-import os
-from utils import load_plugins, save_plugins, get_git_repo_url, PLUGIN_DIR
+from utils import load_plugins, save_plugins, get_git_repo_url, get_existing_plugins_by_type
 
 
 def sync_plugins(force_repo_update=False):
@@ -8,39 +7,34 @@ def sync_plugins(force_repo_update=False):
     new_count = 0
     updated_count = 0
 
-    for plugin_type in ["start", "opt"]:
-        dir_path = os.path.join(PLUGIN_DIR, plugin_type)
-        if not os.path.exists(dir_path):
-            continue
+    for plugin_info in get_existing_plugins_by_type():
+        name = plugin_info["name"]
+        plugin_type = plugin_info["type"]
+        path = plugin_info["path"]
+        repo = get_git_repo_url(path)
 
-        for name in os.listdir(dir_path):
-            plugin_path = os.path.join(dir_path, name)
-            if not os.path.isdir(plugin_path):
-                continue
+        plugin = plugin_dict.get(name)
 
-            plugin = plugin_dict.get(name)
-            repo = get_git_repo_url(plugin_path)
-
-            if plugin is None:
-                plugin_dict[name] = {
-                    "name": name,
-                    "type": plugin_type,
-                    "repo": repo,
-                }
-                new_count += 1
-            else:
-                updated = False
-                if force_repo_update:
-                    plugin["repo"] = repo
-                    updated = True
-                elif repo and "repo" not in plugin:
-                    plugin["repo"] = repo
-                    updated = True
-                if plugin.get("type") != plugin_type:
-                    plugin["type"] = plugin_type
-                    updated = True
-                if updated:
-                    updated_count += 1
+        if plugin is None:
+            plugin_dict[name] = {
+                "name": name,
+                "type": plugin_type,
+                "repo": repo,
+            }
+            new_count += 1
+        else:
+            updated = False
+            if force_repo_update:
+                plugin["repo"] = repo
+                updated = True
+            elif repo and "repo" not in plugin:
+                plugin["repo"] = repo
+                updated = True
+            if plugin.get("type") != plugin_type:
+                plugin["type"] = plugin_type
+                updated = True
+            if updated:
+                updated_count += 1
 
     save_plugins(list(plugin_dict.values()))
     print(
